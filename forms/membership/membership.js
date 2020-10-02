a000d9ec954fabc57642a2b5d6eb343ecdb982b1
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-10-01 22:06:54 -0500

    Update_2

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,19 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+let memberName = ""
+//take in member name
+
+Button1.onclick=function(){
+  memberName = Input1.value
+  let checkName = members.includes(memberName)
+  if (checkName == true) {
+    lblTrue.hidden = false 
+    lblTrue.textContent = "Your username is recognized, please continue."
+  }
+  else { 
+    lblFalse.hidden = false
+    lblFalse.textContent = "Your username does not exist, you will now be registered."
+    members.push(memberName)
+  }
+}
