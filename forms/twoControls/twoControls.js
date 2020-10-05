ca9e49e4b5353654a261ca20b9dd44460b40e65d
Jrp85607 <54645651+Jrp85607@users.noreply.github.com>
2020-10-04 20:07:08 -0500

    update

--- forms/twoControls/twoControls.js
+++ forms/twoControls/twoControls.js
@@ -1,6 +1,11 @@
 
+let name = " "
 
-
-inptSubmit.onclick=function(){
-  
+Dropdown1.onclick=function(s){
+  name = inptName2.value
+  if(typeof(s) == "Login") {
+  return
+  }
+  lblDropdownResponse.hidden = false
+  lblDropdownResponse.textContent = (`${name} selected ${Dropdown1.selection}`)
 }
